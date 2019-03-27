8d37eeabffdb055047a650bdb34173e199c0bf66
Sathwik Tejaswi M <tejaswisathwik@gmail.com>
2019-03-27 05:29:15 -0500

    Update model.py

--- Code/model.py
+++ Code/model.py
@@ -1,85 +1,88 @@
 import torch
-import torch.nn as nn
 import torchvision
-import torchvision.transforms as transforms
-import torchvision.models as models
-from data_utils import *
-from torch.utils.data import Dataset, DataLoader
 import numpy as np
+import torch.nn as nn
+from data_utils import *
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+
 
 print('libraries imoprted')
 # Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('loading_data...')
 # create data loader
-Tiny = Tiny()
-dataloader = DataLoader(Tiny, batch_size=16,shuffle=True,num_workers=10)
+TINY = Tiny()
+DATALOADER = DataLoader(TINY, batch_size=16,shuffle=True,num_workers=10)
 
 print('define hyper parameters and import model')
 # Hyper-parameters
-num_epochs = 6
-learning_rate = 0.0001
+NUM_EPOCHS = 6
+LEARNING_RATE = 0.0001
 
 #create the model
-model  = models.resnet50(pretrained=True)
-num_features = model.fc.in_features
-model.fc = nn.Linear(num_features,2000)
-model = model.to(device)
+MODEL  = models.resnet50(pretrained=True)
+num_features = MODEL.fc.in_features
+MODEL.fc = nn.Linear(num_features,2000)
+MODEL = MODEL.to(DEVICE)
+MODEL_PATH = None # Once you have trained this model and have a checkpoint, replace None with the path to the checkpoint
 
-model.load_state_dict(torch.load('modelresnewnew3.ckpt'))
+try :
+    MODEL.load_state_dict(torch.load(MODEL_PATH))
+except :
+    pass
+
 # Loss and optimizer
-optimizer = torch.optim.SGD(model.parameters(), lr = 0.0001, momentum=0.9)
-
+OPTIMIZER = torch.optim.SGD(MODEL.parameters(), lr = 0.0001, momentum=0.9)
 
 def forward(x):
-    x = x.type('torch.FloatTensor').to(device)
-    return(model(x))
+    x = x.type('torch.FloatTensor').to(DEVICE)
+    return(MODEL(x))
 
 # For updating learning rate
-def update_lr(optimizer, lr):
-    for param_group in optimizer.param_groups:
+def update_lr(OPTIMIZER, lr):
+    for param_group in OPTIMIZER.param_groups:
         param_group['lr'] = lr
 
-Loss_tr = []
-Big_L = []
+LOSS_TR = []
+BIG_L = []
 
 print('begin training')
 # Train the model
-total_step = len(dataloader)
-curr_lr = learning_rate
+TOTAL_STEP = len(DATALOADER)
+CURR_LR = LEARNING_RATE
 
 print('')
 print('')
-for epoch in range(num_epochs):
-    for i, (D, L, IDX) in enumerate(dataloader):
+for epoch in range(NUM_EPOCHS):
+    for i, (D, L, IDX) in enumerate(DATALOADER):
         print(i,end='\r')
         #forward pass
         P = forward(D[0])
         Q = forward(D[1])
         R = forward(D[2])
-
+        # compute loss
         triplet_loss = nn.TripletMarginLoss(margin=1.0, p=2)
         loss = triplet_loss(P,Q,R)
-
-
         # Backward and optimize
-        optimizer.zero_grad()
+        OPTIMIZER.zero_grad()
         loss.backward()
-        Loss_tr.append(loss.item())
-        optimizer.step()
+        LOSS_TR.append(loss.item())
+        OPTIMIZER.step()
         if (i+1) % 100 == 0:
-            temp = sum(Loss_tr)/len(Loss_tr)
-            print ("Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}".format(epoch+1, num_epochs, i+1, total_step, temp))
-            Big_L = Big_L + Loss_tr
-            Loss_tr = []
+            temp = sum(LOSS_TR)/len(LOSS_TR)
+            print ("Epoch [{}/{}], Step [{}/{}] Loss: {:.4f}".format(epoch+1, NUM_EPOCHS, i+1, TOTAL_STEP, temp))
+            BIG_L = BIG_L + LOSS_TR
+            LOSS_TR = []
 
     # Decay learning rate
     if (epoch+1) % 3 == 0:
-        curr_lr /= 1.5
-        update_lr(optimizer, curr_lr)
+        CURR_LR /= 1.5
+        update_lr(OPTIMIZER, CURR_LR)
 
-    torch.save(model.state_dict(), 'MRS'+str(epoch)+'.ckpt')
+    torch.save(MODEL.state_dict(), 'MRS'+str(epoch)+'.ckpt')
     try :
-        np.save('loss_file',Big_L)
+        np.save('loss_file',BIG_L)
     except :
         pass
